72a0ca74c3d04d7ab58b6a74ab9a830e42370aee
Nathaniel R. Lewis <github@nrlewis.dev>
2022-12-11 14:36:04 -0800

    more or less works, but is not performant whatsoever

--- src/main.c
+++ src/main.c
@@ -12,11 +12,11 @@
 
 #include "shaders.h"
 
-#define CMUCAM_IMAGE_WIDTH 81
+#define CMUCAM_IMAGE_WIDTH 80
 #define CMUCAM_IMAGE_HEIGHT 143
 
-int wait_cmucam(int fd) {
-    char byte = '\0';
+int cmucam_wait(int fd) {
+    uint8_t byte = 0;
     do {
         fsync(fd);
         int rc = read(fd, &byte, 1);
@@ -24,40 +24,40 @@ int wait_cmucam(int fd) {
             fprintf(stderr, "failed to read from CMUcam - %d\n", rc);
             return rc;
         } else if (rc > 0) {
-            fprintf(stderr, "got byte: %u\n", byte);
+            // sanitize byte
+            char c = (byte > 31 && byte < 127) ? byte : '.';
+            fprintf(stderr, "got byte: %u (%c)\n", byte, c);
         }
     } while (byte != ':');
     return 0;
 }
 
-int reset_cmucam(int fd) {
-    const char cmucam_reset_cmd[] = {'R', 'S', 0};
-    int rc = write(fd, cmucam_reset_cmd, 3);
+int cmucam_exit_raw_mode(int fd) {
+    const char cmucam_reset_cmd[] = {'R', 'M', 1, 0};
+    int rc = write(fd, cmucam_reset_cmd, 4);
     if (rc < 0) {
         fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
         return rc;
     }
 
-    return wait_cmucam(fd);
+    return cmucam_wait(fd);
 }
 
-int setup_cmucam(const char* path) {
-    // Setup CMUcam
+int cmucam_open(const char* path) {
+    // configure 115200 8n1, no in kernel processing
     int fd = open(path, O_RDWR);
     if (fd < 0) {
-        fprintf(stderr, "failed to open CMUcam - %d\n", fd);
+        perror("failed to open CMUcam");
         return fd;
     }
-    usleep(10000);
 
-    /*struct termios attrs;
+    struct termios attrs;
     int rc = tcgetattr(fd, &attrs);
     if (rc < 0) {
-        fprintf(stderr, "failed to get termios attributes - %d\n", rc);
+        perror("failed to get termios attributes");
         return rc;
     }
 
-    // configure 115200 8n1, no flow control or line processing
     attrs.c_cflag &= ~PARENB;
     attrs.c_cflag &= ~CSTOPB;
     attrs.c_cflag &= ~CSIZE;
@@ -70,46 +70,55 @@ int setup_cmucam(const char* path) {
     attrs.c_lflag &= ~ECHOE;
     attrs.c_lflag &= ~ECHONL;
     attrs.c_lflag &= ~ISIG;
+    attrs.c_lflag &= ~ECHOK;
+    attrs.c_lflag &= ~IEXTEN;
+    attrs.c_lflag &= ~ECHOCTL;
+    attrs.c_lflag &= ~ECHOKE;
 
     attrs.c_iflag &= ~(IXON | IXOFF | IXANY);
     attrs.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+
     attrs.c_oflag &= ~OPOST;
     attrs.c_oflag &= ~ONLCR;
 
-    //attrs.c_cc[VTIME] = 0;
-    //attrs.c_cc[VMIN] = 0;
+    attrs.c_cc[VTIME] = 10;
+    attrs.c_cc[VMIN] = 0;
 
     cfsetispeed(&attrs, B115200);
     cfsetospeed(&attrs, B115200);
 
     rc = tcsetattr(fd, TCSANOW, &attrs);
     if (rc < 0) {
-        fprintf(stderr, "failed to get termios attributes - %d\n", rc);
+        fprintf(stderr, "failed to set termios attributes - %d\n", rc);
         return rc;
-    }*/
+    }
 
+    return fd;
+}
+
+int cmucam_setup(int fd) {
     // Boot the camera (get a prompt)
-    /*const char* cmucam_boot_cmd = "\r";
+    const char* cmucam_boot_cmd = "\r";
     int rc = write(fd, &cmucam_boot_cmd, 1);
     if (rc < 0 || rc != 1) {
         fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
     }
 
-    rc = wait_cmucam(fd);
+    rc = cmucam_wait(fd);
     if (rc < 0) {
         return rc;
-    }*/
+    }
     printf("CMUcam booted\n");
 
     // Put the camera in raw mode
     const char* cmucam_rawmode_cmd = "RM 7\r";
-    int rc = write(fd, cmucam_rawmode_cmd, 5);
+    rc = write(fd, cmucam_rawmode_cmd, 5);
     if (rc < 0 || rc != 5) {
         fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
         return rc;
     }
 
-    rc = wait_cmucam(fd);
+    rc = cmucam_wait(fd);
     if (rc < 0) {
         return rc;
     }
@@ -119,13 +128,18 @@ int setup_cmucam(const char* path) {
 }
 
 int main(int argc, char** argv) {
-    int cmucam = setup_cmucam("/dev/ttyUSB0");
+    int cmucam = cmucam_open("/dev/ttyUSB0");
     if (cmucam < 0) {
         return EXIT_FAILURE;
     }
 
+    int rc = cmucam_setup(cmucam);
+    if (rc < 0) {
+        return EXIT_FAILURE;
+    }
+
     // Setup an OpenGL 4.3 rendering environment
-    int rc = SDL_Init(SDL_INIT_VIDEO);
+    rc = SDL_Init(SDL_INIT_VIDEO);
     if (rc < 0) {
         fprintf(stderr, "failed to initialize SDL with error: %s (%d)\n", SDL_GetError(), -rc);
         return EXIT_FAILURE;
@@ -209,7 +223,7 @@ int main(int argc, char** argv) {
     glBindTexture(GL_TEXTURE_2D, 0);
 
     SDL_GL_SetSwapInterval(0);
-    glClearColor(0, 0, 0, 1);
+    SDL_ShowWindow(window);
     
     // Start CMUcam image dump
     const char cmucam_dumpframe_cmd[] = {'D', 'F', 0};
@@ -220,19 +234,14 @@ int main(int argc, char** argv) {
     }
     
     // Draw CMUcam image until exit is requested
-    SDL_ShowWindow(window);
-    GLint column = 0;
+    uint8_t image[CMUCAM_IMAGE_HEIGHT * CMUCAM_IMAGE_WIDTH * 3];
+    GLint x = 0;
+    GLint y = 0;
+    GLint c = 0;
     bool quit = false;
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
-                printf("Exit requested\n");
-                quit = true;
-            }
-        }
 
-        // process incoming frame from CMUcam
+    while (true) {
+        // fetch a byte from the camera
         uint8_t b = 0;
         rc = 0;
         do {
@@ -245,16 +254,21 @@ int main(int argc, char** argv) {
 
         if (b == 1) {
             // new frame
-            column = 0;
-            printf("column 0\n");
-        } else if (b == 2) {
-            column++;
-            printf("column %d\n", column);
-        } else if (b == 3) {
-            // discard
+            x = 0;
+            y = 0;
+            c = 0;
+            //printf("column 0\n");
             continue;
-        } else if (b == ':') {
-            printf("completed frame\n");
+        } else if (b == 2) {
+            x++;
+            y = 0;
+            c = 0;
+            //printf("column %d\n", x);
+        } else if (b == 3) {
+            cmucam_wait(cmucam);
+            if (quit) {
+                break;
+            }
 
             // start next frame
             rc = write(cmucam, &cmucam_dumpframe_cmd, 3);
@@ -263,30 +277,35 @@ int main(int argc, char** argv) {
                 return EXIT_FAILURE;
             }
             continue;
+        } else {
+            image[3*(y*CMUCAM_IMAGE_WIDTH + x) + c] = b;
+            if (c < 2) {
+                c++;
+            } else {
+                c = 0;
+                y++;
+            }
         }
 
-        // read column
-        uint8_t column_data[429];
-        size_t offset = 0;
-        while (offset != sizeof(column_data)) {
-            rc = read(cmucam, column_data + offset, sizeof(column_data) - offset);
-            if (rc < 0) {
-                fprintf(stderr, "failed to read from CMUcam (2) - %d\n", rc);
-                return EXIT_FAILURE;
+        SDL_Event event;
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                printf("Exit requested\n");
+                quit = true;
             }
-            offset += rc;
         }
 
         // update texture
         glBindTexture(GL_TEXTURE_2D, frame);
-        glTexSubImage2D(GL_TEXTURE_2D, 0, column, 0, 1, CMUCAM_IMAGE_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, column_data);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, CMUCAM_IMAGE_WIDTH, CMUCAM_IMAGE_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, image);
 
         // redraw window
         int width;
         int height;
         SDL_GetWindowSize(window, &width, &height);
-
         glViewport(0, 0, width, height);
+
+        glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(quad_shader_prog);
         glBindVertexArray(quadVAO);
@@ -297,6 +316,6 @@ int main(int argc, char** argv) {
         SDL_GL_SwapWindow(window);
     }
 
-    reset_cmucam(cmucam);
+    cmucam_exit_raw_mode(cmucam);
     return EXIT_SUCCESS;
 }
