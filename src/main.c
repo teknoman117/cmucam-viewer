a659b6aa1ffef546d990463a3fcab2497db4d7c0
Nathaniel R. Lewis <github@nrlewis.dev>
2022-12-11 03:55:26 -0800

    starting to work. pretty broken but starting to show an image

--- src/main.c
+++ src/main.c
@@ -6,13 +6,124 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <inttypes.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 
 #include "shaders.h"
 
 #define CMUCAM_IMAGE_WIDTH 81
 #define CMUCAM_IMAGE_HEIGHT 143
 
+int wait_cmucam(int fd) {
+    char byte = '\0';
+    do {
+        fsync(fd);
+        int rc = read(fd, &byte, 1);
+        if (rc < 0) {
+            fprintf(stderr, "failed to read from CMUcam - %d\n", rc);
+            return rc;
+        } else if (rc > 0) {
+            fprintf(stderr, "got byte: %u\n", byte);
+        }
+    } while (byte != ':');
+    return 0;
+}
+
+int reset_cmucam(int fd) {
+    const char cmucam_reset_cmd[] = {'R', 'S', 0};
+    int rc = write(fd, cmucam_reset_cmd, 3);
+    if (rc < 0) {
+        fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
+        return rc;
+    }
+
+    return wait_cmucam(fd);
+}
+
+int setup_cmucam(const char* path) {
+    // Setup CMUcam
+    int fd = open(path, O_RDWR);
+    if (fd < 0) {
+        fprintf(stderr, "failed to open CMUcam - %d\n", fd);
+        return fd;
+    }
+    usleep(10000);
+
+    /*struct termios attrs;
+    int rc = tcgetattr(fd, &attrs);
+    if (rc < 0) {
+        fprintf(stderr, "failed to get termios attributes - %d\n", rc);
+        return rc;
+    }
+
+    // configure 115200 8n1, no flow control or line processing
+    attrs.c_cflag &= ~PARENB;
+    attrs.c_cflag &= ~CSTOPB;
+    attrs.c_cflag &= ~CSIZE;
+    attrs.c_cflag |= CS8;
+    attrs.c_cflag &= ~CRTSCTS;
+    attrs.c_cflag |= CREAD | CLOCAL;
+
+    attrs.c_lflag &= ~ICANON;
+    attrs.c_lflag &= ~ECHO;
+    attrs.c_lflag &= ~ECHOE;
+    attrs.c_lflag &= ~ECHONL;
+    attrs.c_lflag &= ~ISIG;
+
+    attrs.c_iflag &= ~(IXON | IXOFF | IXANY);
+    attrs.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+    attrs.c_oflag &= ~OPOST;
+    attrs.c_oflag &= ~ONLCR;
+
+    //attrs.c_cc[VTIME] = 0;
+    //attrs.c_cc[VMIN] = 0;
+
+    cfsetispeed(&attrs, B115200);
+    cfsetospeed(&attrs, B115200);
+
+    rc = tcsetattr(fd, TCSANOW, &attrs);
+    if (rc < 0) {
+        fprintf(stderr, "failed to get termios attributes - %d\n", rc);
+        return rc;
+    }*/
+
+    // Boot the camera (get a prompt)
+    /*const char* cmucam_boot_cmd = "\r";
+    int rc = write(fd, &cmucam_boot_cmd, 1);
+    if (rc < 0 || rc != 1) {
+        fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
+    }
+
+    rc = wait_cmucam(fd);
+    if (rc < 0) {
+        return rc;
+    }*/
+    printf("CMUcam booted\n");
+
+    // Put the camera in raw mode
+    const char* cmucam_rawmode_cmd = "RM 7\r";
+    int rc = write(fd, cmucam_rawmode_cmd, 5);
+    if (rc < 0 || rc != 5) {
+        fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
+        return rc;
+    }
+
+    rc = wait_cmucam(fd);
+    if (rc < 0) {
+        return rc;
+    }
+    printf("CMUcam in raw mode\n");
+
+    return fd;
+}
+
 int main(int argc, char** argv) {
+    int cmucam = setup_cmucam("/dev/ttyUSB0");
+    if (cmucam < 0) {
+        return EXIT_FAILURE;
+    }
+
     // Setup an OpenGL 4.3 rendering environment
     int rc = SDL_Init(SDL_INIT_VIDEO);
     if (rc < 0) {
@@ -60,10 +171,10 @@ int main(int argc, char** argv) {
     }
 
     const GLfloat quad[] = {
-            -1.0f, -1.0f, 0.0f, 1.0f,
-            -1.0f,  1.0f, 0.0f, 0.0f,
-            1.0f, -1.0f, 1.0f, 1.0f,
-            1.0f,  1.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f,
+            -1.0f,  1.0f, 0.0f, 1.0f,
+            1.0f, -1.0f, 1.0f, 0.0f,
+            1.0f,  1.0f, 1.0f, 1.0f,
     };
 
     GLuint quadVAO;
@@ -100,8 +211,17 @@ int main(int argc, char** argv) {
     SDL_GL_SetSwapInterval(0);
     glClearColor(0, 0, 0, 1);
     
+    // Start CMUcam image dump
+    const char cmucam_dumpframe_cmd[] = {'D', 'F', 0};
+    rc = write(cmucam, &cmucam_dumpframe_cmd, 3);
+    if (rc < 0 || rc != 3) {
+        fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
+        return EXIT_FAILURE;
+    }
+    
     // Draw CMUcam image until exit is requested
     SDL_ShowWindow(window);
+    GLint column = 0;
     bool quit = false;
     while (!quit) {
         SDL_Event event;
@@ -113,6 +233,53 @@ int main(int argc, char** argv) {
         }
 
         // process incoming frame from CMUcam
+        uint8_t b = 0;
+        rc = 0;
+        do {
+            rc = read(cmucam, &b, 1);
+            if (rc < 0) {
+                fprintf(stderr, "failed to read from CMUcam (1) - %d\n", rc);
+                return EXIT_FAILURE;
+            }
+        } while (rc != 1);
+
+        if (b == 1) {
+            // new frame
+            column = 0;
+            printf("column 0\n");
+        } else if (b == 2) {
+            column++;
+            printf("column %d\n", column);
+        } else if (b == 3) {
+            // discard
+            continue;
+        } else if (b == ':') {
+            printf("completed frame\n");
+
+            // start next frame
+            rc = write(cmucam, &cmucam_dumpframe_cmd, 3);
+            if (rc < 0 || rc != 3) {
+                fprintf(stderr, "failed to write to CMUcam - %d\n", rc);
+                return EXIT_FAILURE;
+            }
+            continue;
+        }
+
+        // read column
+        uint8_t column_data[429];
+        size_t offset = 0;
+        while (offset != sizeof(column_data)) {
+            rc = read(cmucam, column_data + offset, sizeof(column_data) - offset);
+            if (rc < 0) {
+                fprintf(stderr, "failed to read from CMUcam (2) - %d\n", rc);
+                return EXIT_FAILURE;
+            }
+            offset += rc;
+        }
+
+        // update texture
+        glBindTexture(GL_TEXTURE_2D, frame);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, column, 0, 1, CMUCAM_IMAGE_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, column_data);
 
         // redraw window
         int width;
@@ -124,12 +291,12 @@ int main(int argc, char** argv) {
         glUseProgram(quad_shader_prog);
         glBindVertexArray(quadVAO);
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, frame);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
         glBindTexture(GL_TEXTURE_2D, 0);
         glBindVertexArray(0);
         SDL_GL_SwapWindow(window);
     }
 
+    reset_cmucam(cmucam);
     return EXIT_SUCCESS;
 }
